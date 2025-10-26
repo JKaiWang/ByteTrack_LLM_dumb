27eae60aff2a912e7c9e7dcdbb4216d164d6740b
JKaiWang <a0988347904@gmail.com>
2025-10-26 19:24:16 +0800

    update: LLM.pu run.py

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
 import subprocess
 import argparse
+import os, sys
+
+# 取得當前 run.py 所在的根目錄
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# 把根目錄和 yolox 資料夾加進 Python 模組搜尋路徑
+sys.path.append(ROOT_DIR)
+sys.path.append(os.path.join(ROOT_DIR, "yolox"))
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--prompt", type=str, required=True, help="Prompt 給 LLM，例如：找出穿粉紅色衣服的人")
